11b2d1b75042435db4c1f1aae09d952bef56eb76
yolodion <diony5338@gmail.com>
2022-12-21 14:37:44 +0900

    create: 리액트 라우터 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <div>Not found</div>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <div>프로젝트</div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
