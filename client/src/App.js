9a0aa4cc02ab28ab3ccada704e0fa5d2a9bfc9d8
Sodasom <1219dasom@gmail.com>
2022-12-22 17:02:33 +0900

    feat: header 영역 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,54 @@
-import "./App.css";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import SignupPage from "./pages/SignupPage/SignupPage";
-import QuestionListPage from "./pages/QuestionListPage/QuestionListPage";
-import QuestionDetailPage from "./pages/QuestionDetailPage/QuestionDetailPage";
-import AskPage from "./pages/AskPage/AskPage";
-import MyPage from "./pages/MyPage/MyPage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './App.css';
+import LoginPage from './pages/LoginPage/LoginPage';
+import SignupPage from './pages/SignupPage/SignupPage';
+import QuestionListPage from './pages/QuestionListPage/QuestionListPage';
+import QuestionDetailPage from './pages/QuestionDetailPage/QuestionDetailPage';
+import AskPage from './pages/AskPage/AskPage';
+import MyPage from './pages/MyPage/MyPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Nav from './components/Nav/Nav';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <QuestionListPage />,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LoginPage />,
     errorElement: <div>Not found</div>,
   },
   {
-    path: "/signup",
+    path: '/signup',
     element: <SignupPage />,
   },
   {
-    path: "/questions",
+    path: '/questions',
     element: <QuestionListPage />,
   },
   {
-    path: "/questions/ask",
+    path: '/questions/ask',
     element: <AskPage />,
   },
   {
-    path: "/questions/:id",
+    path: '/questions/:id',
     element: <QuestionDetailPage />,
   },
   {
-    path: "/mypage",
+    path: '/mypage',
     element: <MyPage />,
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
