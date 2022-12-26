21f4f3f0473f5f231377c5637d4a48907c79158d
Sangyoo <tkddbwkd@naver.com>
2022-12-26 14:57:36 +0900

    chore: question 엔티티 import 수정

--- deployServer/src/main/java/com/codestates/pre_project/vote/answerVote/service/AnswerVoteService.java
+++ deployServer/src/main/java/com/codestates/pre_project/vote/answerVote/service/AnswerVoteService.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/* AnswerVote service
+* 처음에 만들때 createAnswerVote 한번 하고
+* 그 다음부터 update로 조정한다.
+* */
 //@Service
 //@RequiredArgsConstructor
 //public class AnswerVoteService {
@@ -18,16 +22,22 @@ import java.util.Optional;
 //    private final AnswerVoteRepository avRepository;
 //    private final MemberRepository memberRepository;
 //
-//    public AnswerVote createAnswerVote(AnswerVote av){}
-//    public AnswerVote updateAnswerVote(AnswerVote av) {}
+//    public AnswerVote createAnswerVote(AnswerVote av){
+//        // 만들 때는 멤버를 확인하지 않는다
+//        return avRepository.save(av);
+//    }
+//    public AnswerVote updateAnswerVote(AnswerVote av) {
+//        // 업데이트 할때 이미 투표한 멤버인지 확인한다.
+//    }
 //    public AnswerVote findAnswerVote(long avId){}
 //    public Page<AnswerVote> findAnswerVotes(int page, int size){}
 //    public void deleteAnswerVote(long avId) {}
 //    private AnswerVote findVerifiedAV(long avId){}
 //
-//    // 이미 체크한 회원인지 확인 true -> 체크한 회원 false->
+//    // 이미 체크한 회원인지 확인 true -> 체크한 회원 false-> 이번에 처음 체크한 회원
 //    private boolean verifyAlreadyCheckedMember(AnswerVote av){
 //         Optional<Member> optionalMember = memberRepository.findById(av.getMember().getMemberId());
 //         if(optionalMember.isPresent()) return true;
+//         else return false;
 //    }
 //}
